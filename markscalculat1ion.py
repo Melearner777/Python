b557eb092defa4a8b1fcaaaedb653af91568949d
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-01-17 19:15:48 +0530

    Add files via upload

---
+++ markscalculat1ion.py
@@ -0,0 +1,9 @@
+marks=float(input("enter the marks:"))
+if marks >80:
+    print("very good")
+elif marks >60:
+     print("good")
+elif marks >40:
+     print("good")
+else :
+     print("fail")          
