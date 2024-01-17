02b449a165b32381270e91f0777aae2d4cb27c02
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-01-17 19:15:27 +0530

    Add files via upload

---
+++ logicaloperators.py
@@ -0,0 +1,10 @@
+a=float(input("enter  first number:"))
+b=float(input("enter second number:"))
+ 
+# Comparison Operators
+print("a == b:", a == b)  # Output: a == b: False
+print("a != b:", a != b)  # Output: a != b: True
+print("a < b:", a < b)    # Output: a < b: False
+print("a > b:", a > b)    # Output: a > b: True
+print("a <= b:", a <= b)  # Output: a <= b: False
+print("a >= b:", a >= b)  # Output: a >= b: True
