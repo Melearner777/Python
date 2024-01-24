0138a97062f98537825a50134ef0dd096a54123d
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-01-24 15:02:01 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,8 @@
+name="preetam singh"
+roll_no=21
+percentage=99.9
+place_live="greater noida"
+#print(name,roll_no,percentage,place_live)
+print("my name is",name,"my roll no is",roll_no,
+      "i scored",percentage,"%in exam","and i lived in",
+      place_live)
