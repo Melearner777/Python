33aac068d6b0932de45b9f74ab224eedbcecaac2
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-02-04 18:25:41 +0530

    Add files via upload

---
+++ bool.py
@@ -0,0 +1,49 @@
+'''print(bool(1))
+True
+print(bool(0))
+False
+
+print(bool("Galgotia"))
+True
+print(bool(""))
+False'''
+'''age=int(input("Enter your age"))
+if age >=  18:
+   print("Elegible for voting")
+else:
+   print("Eligible for not voting")
+if (1):
+   print("hlo")'''
+
+'''n1=int(input("Enter first number: "))
+n2=int(input("Enter second number: "))
+n3=int(input("Enter third number: "))
+
+if n1>n2:
+    print("n1 is greater")
+
+elif n2>n3:
+    print("n2 is greater")
+
+else:
+    print("n3 is greater")'''
+'''M=input("Enter your letter")
+if ((M=="a") or (M=="e") or (M=="i") or (M=="o") or (M=="u") or
+   (M=="A") or (M=="E") or (M=="I") or (M=="O") or (M=="U")):
+    
+    print(f" {M} is vowel")
+else:
+    print(f" {M} is consonant")'''
+
+'''X=input("Enter your letter")
+U="aeiouAEIOU"
+print(U.find (X))'''
+X=int(input("Enter your year"))
+if ((X/4) or
+  (X is not divisible by 100) or
+  (X is not divisible by 400))
+    print("Leap year")
+
+else:
+   print("None")
+
