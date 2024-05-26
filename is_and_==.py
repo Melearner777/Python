2bffb7138a22e1d2d5d66ad2f685bc0bf7856b6a
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-05-26 22:38:17 +0530

    Add files via upload

---
+++ is_and_==.py
@@ -0,0 +1,11 @@
+a=3
+b=3
+c=[1,2,3]
+d=[1,2,3]
+e=None
+
+print(a is b) #Exact location of object in memory
+print(a==b) #value
+print(c is d)
+print(c==d)
+print(e is None) #Excat location of object in memory
